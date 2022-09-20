4b3ef368e6a1af447e1da7c18b9e337afff9d1a8
Tom Bebbington <thomas.bebbington@yellowgrid.co.uk>
2022-09-20 14:49:35 +0100

    Fix page sidebar handling

--- src/Page/Page.tsx
+++ src/Page/Page.tsx
@@ -1,22 +1,28 @@
-import { children, createSignal, JSXElement } from "solid-js";
+import { children, createSignal, JSXElement, Show } from "solid-js";
 import { createStore } from "solid-js/store";
 
-export  interface PageProps {
-    children: JSXElement
-    header: JSXElement
-    sidebar?: JSXElement
-    showSidebar: boolean
+export interface PageProps {
+  children: JSXElement
+  header: JSXElement
+  sidebar?: JSXElement
+  showSidebar: boolean
 }
 
 export function Page(raw: PageProps) {
   const [props] = createStore(raw)
-  const c= children(() => props.children);
-    return <div class="pf-c-page">
-      {c()}
+  const c = children(() => props.children);
+  return <div class="pf-c-page">
+    {c()}
     {props.header}
-    <div classList={{'pf-c-page__sidebar': true, ['pf-m-'+(props.showSidebar ? 'expanded' : 'collapsed')]: true}}>
-      <div class="pf-c-page__sidebar-body">pf-c-nav</div>
-    </div>
+
+    <Show when={props.sidebar}>
+      <div style='color:white' classList={{ 'pf-c-page__sidebar': true, ['pf-m-' + (props.showSidebar ? 'expanded' : 'collapsed')]: true}}>
+        <div class="pf-c-page__sidebar-body">
+          {props.sidebar}
+        </div>
+      </div>
+
+    </Show>
     <main class="pf-c-page__main" tabindex="-1">
       {c()}
     </main>
